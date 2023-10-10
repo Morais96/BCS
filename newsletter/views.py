8dff932c9932b63106c84d72228648ab2a3e90cc
Luis Morais <ladspm2013@gmail.com>
2023-10-10 16:03:43 +0100

    final

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from .forms import SubscriberForm
 from .models import Subscriber
 from django.http import JsonResponse
+from django.template.loader import render_to_string
+from email.mime.image import MIMEImage
+from django.conf import settings
+import os
+
+def logo_data(image):
+    path = os.path.join(settings.MEDIA_ROOT, image)
+    with open(path, 'rb') as f:
+        logo_data = f.read()
+    logo = MIMEImage(logo_data)
+    return logo
 
 def coming_soon_page(request):
 
@@ -14,11 +25,20 @@ def coming_soon_page(request):
         
         if form.is_valid():
             form.save()
-            subject = 'Welcome to Burriqueira'
-            message = 'Thank you for subscribing to our newsletter!'
-            from_email = 'support@burriqueira.com'
-            recipient_list = [email]
-            send_mail(subject, message, from_email, recipient_list)
+            
+            message = EmailMultiAlternatives(
+                subject='Welcome to Burriqueira',
+                body=render_to_string('welcome_email.html', {}).strip(),
+                from_email='support@burriqueira.com',
+                to=[email]
+            )
+            message.content_subtype = 'html'
+            message.mixed_subtype = 'related'
+            message.attach(logo_data('00011.jpeg'))
+            message.attach(logo_data('00009.jpeg'))
+
+            message.send(fail_silently=False)
+
             return JsonResponse({'valid': True, 'subscribed': False})
         else:
             return JsonResponse({'valid': False, 'subscribed': False})
